bf39415f80b419a61f637e43ff78e349e5bdadb8
endzju <andrebaronstudent@gmail.com>
2026-05-06 17:32:43 +0200

    drawing boxes

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 Download dataset from:
 
 https://drive.google.com/drive/folders/18ZhDu8w9Ph-JqIXXbOvolcKLwRW5vKeu
+
+How to run?
+
+```bash
+    uv sync
+    uv run python .\submission\source\main.py
+```
--- submission/source/main.py
+++ submission/source/main.py
@@ -1,4 +1,14 @@
+from collections import defaultdict
 from pathlib import Path
+
+import cv2
+
+
+def draw_detections(image, detection_list):
+    for box in detection_list:
+        _, x, y, w, h, _ = box
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
 
 project_path = Path(__file__).parent.parent.parent
 MOT_folder = project_path / "evs_mot-train" / "MOT_05"
@@ -9,20 +19,32 @@ with open(MOT_folder / "seqinfo.ini") as f:
     for line in f:
         if "=" in line:
             key, value = line.strip().split("=")
+            if key in ["frameRate", "seqLength", "imWidth", "imHeight"]:
+                value = int(value)
             seqinfo[key] = value
 
 det_path = MOT_folder / "det" / "det.txt"
 
-det = {}
+det = defaultdict(list)
 
 with open(det_path) as f:
     for line in f:
         frame, *info = line.strip().split(",")
-        info = [float(i) for i in info]
-        det[int(frame)] = info
+        info = [round(float(i)) for i in info]
+        det[int(frame) - 1].append(info)
 
 images = []
 
-for frame in range(1, int(seqinfo["frames"] + 1)):
+print("Loading images...")
+for frame in range(1, seqinfo["seqLength"] + 1):
     image_path = MOT_folder / seqinfo["imDir"] / f"{frame:06d}{seqinfo['imExt']}"
-    images.append(image_path)
+    img = cv2.imread(image_path)
+    images.append(img)
+
+print("Drawing boxes...")
+
+draw_detections(images[0], det[0])
+
+cv2.imshow("Obraz", images[0])
+cv2.waitKey(0)
+cv2.destroyAllWindows()
